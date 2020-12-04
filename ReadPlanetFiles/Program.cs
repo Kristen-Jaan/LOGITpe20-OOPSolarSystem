5a0313d8838c3d3710a6a4628310a5541f2d5886
Opilane TTHK <opilane@tthk.ee>
2020-12-04 10:06:44 +0200

    first commit

---
+++ ReadPlanetFiles/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace ReadPlanetFiles
+{
+    class Program
+    {
+        public class Planets
+        {
+            string name;
+            int mass;
+
+            public Planets(string _name, int _mass)
+            {
+                name = _name;
+                mass = _mass;
+            }
+
+            public string Name { get { return name; } }
+            public int Mass { get { return mass; } }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Help Me!");
+            WritePlanetList();
+        }
+
+        public static void WritePlanetList()
+        {
+            List<string> planetList = new List<string>();
+            for(int i = 0; i < 9; i++)
+            {
+                Console.WriteLine("Enter a planet: ");
+                string planet = Console.ReadLine();
+                Console.WriteLine("What's the mass?");
+                string mass = Console.ReadLine();
+
+                string line = $"{planet}/{mass}";
+                planetList.Add(line);
+            }
+
+            string filePath = @"C:\Users\opilane\Samples";
+            string fileName = @"planets.txt";
+            File.WriteAllLines(Path.Combine(filePath, fileName), planetList);
+        }
+    }
+}
---
+++ ReadPlanetFilesRight/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace ReadPlanetFilesRight
+{
+    class Program
+    {
+        public class Planets
+        {
+            string name;
+            int mass;
+
+            public Planets(string _name, int _mass)
+            {
+                name = _name;
+                mass = _mass;
+            }
+
+            public string Name { get { return name; } }
+            public int Mass { get { return mass; } }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Help Me!");
+            ReadPlanetFiles();
+        }
+
+        public static void ReadPlanetFiles()
+        {
+            string filePath = @"C:\Users\opilane\Samples";
+            string fileName = @"planets.txt";
+
+            List<Planets> planets = new List<Planets>();
+
+            List<string> linesFromFile = File.ReadAllLines(Path.Combine(filePath, fileName)).ToList();
+
+            int total = 0;
+
+            foreach(Planets planet in planets)
+            {
+                Console.WriteLine($"Planet: {planet.Name}; Mass: {planet.Mass}");
+                total += planet.Mass;
+            }
+            Console.WriteLine($"Total mass: {total}");
+
+
+        }
+
+       
+
+        
+    }
+}
